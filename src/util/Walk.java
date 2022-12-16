c8825ee6ef25688982a2a27e8f97cfc3a17120b1
bilaly <billycsk@gmail.com>
2022-12-16 16:13:29 +0100

    Rajout de la fonction move

--- src/util/Walk.java
+++ src/util/Walk.java
@@ -1,9 +1,30 @@
 package util;
 
 public class Walk {
-	public static final void play() {
-		int arrived;
-		int departure = Constants.DEPARTURE;
-		int cast = RandomDice.getRandomNumber();
+	int arrived;
+	int exceed;
+	int target = Constants.TARGET;
+	int departure = Constants.DEPARTURE;
+	int cast = RandomDice.getRandomNumber();
+
+	public void play() {
+
+		arrived = departure + cast;
+		System.out.println("le joueur se trouve à position" + arrived);
+	}
+
+	public void move(int arrived) {
+		if (arrived < target) {
+			cast = RandomDice.getRandomNumber();
+			arrived += cast;
+			System.out.println("le joueur se trouve à position" + arrived);
+		} else if (arrived > target) {
+			exceed = arrived - target;
+			arrived = target - exceed;
+			cast = RandomDice.getRandomNumber();
+			arrived += cast;
+		} else {
+			System.out.println("Bravooo!!!!! vous avez atteint l'objectif");
+		}
 	}
 }
