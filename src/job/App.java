bb4b7b06a05eeac988209cbe9014a32aefcbbcf7
bilaly <billycsk@gmail.com>
2022-12-17 11:01:45 +0100

    Optimisation du code dans la class App et suppression des imports inutilisés

--- src/job/App.java
+++ src/job/App.java
@@ -1,18 +1,14 @@
 package job;
 
-import java.util.ArrayList;
 import model.Player;
 import util.*;
 
 public class App {
 	public static void main(String[] args) {
-		ArrayList<Player> pl = new ArrayList<Player>();
-		System.out.println("Veuillez saisir vos coordonées");
+
+		System.out.println("Veuillez saisir prénom, nom et identifiants dans l'ordre");
 		Player player1 = new Player();
-		pl.add(player1);
-
 		System.out.println(player1);
-
 		Walk w = new Walk();
 		w.throwDice(player1);
 	}
