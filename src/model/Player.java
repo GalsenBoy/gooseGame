2bc7f85daae44bcab35ca00155c80cce229da22a
bilaly <billycsk@gmail.com>
2022-12-19 11:22:23 +0100

    Rajout de la fonction pour recupérer le choix du joueur ainsi que ces accesseur

--- src/model/Player.java
+++ src/model/Player.java
@@ -6,10 +6,12 @@ import java.io.IOException;
 
 //Déclaration des variables
 public class Player {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private String firstName;
 	private String lastName;
 	private String id;
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private int choice;
+	private int score;
 
 	// Saisie au clavier des informations du joueur
 	// Levée d'exception en cas de saisie incorrecte des informations
@@ -25,6 +27,19 @@ public class Player {
 
 	/**
 	 * 
+	 * @return retourne la valeur (choix) saisie par l'utilisateur
+	 */
+	public int enterChoice() {
+		try {
+			this.choice = Integer.parseInt(br.readLine());
+		} catch (IOException e) {
+			System.err.println("Veuillez saisir un nombre entier");
+		}
+		return this.choice;
+	}
+
+	/**
+	 * 
 	 * @return retourne le prénom du joueur
 	 */
 	public String getFirstName() {
@@ -33,7 +48,7 @@ public class Player {
 
 	/**
 	 * 
-	 * @param firstName permet la saisie du prénom du joueur
+	 * @param firstName permet de modifier le prénom du joueur
 	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
@@ -49,7 +64,7 @@ public class Player {
 
 	/**
 	 * 
-	 * @param firstName permet la saisie du nom du joueur
+	 * @param firstName permet de modifier le nom du joueur
 	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
@@ -65,15 +80,31 @@ public class Player {
 
 	/**
 	 * 
-	 * @param firstName permet la saisie de l'identifiant du joueur
+	 * @param firstName permet de modifier l'identifiant du joueur
 	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	/**
+	 * 
+	 * @return retourne le score du joueur
+	 */
+	public int getScore() {
+		return this.score;
+	}
+
+	/**
+	 * 
+	 * @param score permet de modifier la valeur du score
+	 */
+	public void setScore(int score) {
+		this.score = score;
+	}
+
 	// Affiche les informations du joueur : nom,prénom et identifiant
 	@Override
 	public String toString() {
-		return "Prénom : " + firstName + " , " + " Nom : " + lastName + " , " + " Id : " + id + " va se lancer";
+		return "Prénom : " + firstName + " , " + " Nom : " + lastName + " , " + " Id : " + id + " : va se lancer";
 	}
 }
